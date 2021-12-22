809f495703c8c7d9c9f6ebef101bfd36f1016243
Your Name <alla7gon@gmail.com>
2021-12-22 15:08:55 +0200

    innerhtml

--- hw_1/task1/main.js
+++ hw_1/task1/main.js
@@ -8,18 +8,20 @@
 let arr = ["Tom", "Steve", "Bill", "Rita", "Pete", "Ashley"];
 const list = document.getElementById('list');
 
-function createList(arr) {
-
-}
-
-
 function map(arr, f) {
   let result = [];
   for (let el of arr) {
       result.push(f(el));
   } return result;
 } 
+
+function createList(arr) {
+  let item = document.createElement('li');
+  list.appendChild(item);	
+  item.textContent = `${arr}`;
+  return list;
+}
 // console.log(map([1, 2, 3], item => item * 2)); // Має вивести [2, 4, 6]
-// map(arr, createList);
+map(arr, createList);
 
 
