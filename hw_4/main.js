480b557cf37bb309f618bc1407af1a0c68c9280d
Your Name <alla7gon@gmail.com>
2022-01-10 13:51:35 +0200

    hw_4/

---
+++ hw_4/main.js
@@ -0,0 +1,74 @@
+// Завдання 1:
+// Вивести на сторінці 10 постів з ресурсу👉 https://jsonplaceholder.typicode.com/posts
+// Виводити:
+//      - заголовок поста
+//     - посилання на автора цього поста
+// <span class = "author-link"> Get author </ span>
+//      При кліці на посилання завантажити з сервера дані автора поста, вивести під посиланням:
+//       - ім'я user
+//       - кількість постів цього user
+// const posts = document.getElementById ("posts");
+
+function makeRequest() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    return response.json()
+  })
+  .then(function (data) {
+    renderList(data)
+  })
+  .catch(error => console.log('Error', error));
+}
+
+function renderList(data) {
+  const newArr = data.slice(0,10);
+  console.log(newArr)
+  const list = document.querySelector('#list');
+  for (let item of newArr) {
+    list.insertAdjacentHTML('afterbegin', `<li class="item">Title: ${item.title}; <span class="author-link"> Get author </span></li>`)
+  }
+}
+makeRequest();
+
+
+
+// Завдання 2:
+// Провести refactoring коду, використовуючи Promise👇 і щоб виклик функцій був наступним (наведено, ще нижче)
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+xhr.onreadystatechange = handleResponse;
+xhr.send();
+
+function handleResponse(response) {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject('Error');
+  }
+}
+
+function getJSON(url) {
+  return fetch(url)
+    .then(handleResponse)
+    .then(r => r.json());
+}
+
+function generateListItems(users) {
+  return users
+    .map((u) => `<li class="list-group-item">${u.name}</li>`)
+    .join("");
+}
+
+function generateUnorderedList(listItems) {
+  return `<ul class="list-group">${listItems}</ul>`;
+}
+
+function addUsersToPage(users) {
+  document.getElementById("users").innerHTML = users;
+}
+// Щоб виклик функцій був наступним 👇
+
+getJSON("https://jsonplaceholder.typicode.com/users")
+.then(generateListItems)
+.then(generateUnorderedList)
+.then(addUsersToPage);
